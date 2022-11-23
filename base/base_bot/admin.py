5755dde11630a2613e003a4c8c6a63feb2748edc
Vladuslav Dragushin <maxhc@inbox.ru>
2022-11-23 13:16:49 +0300

    Added Cake model .

--- base/base_bot/admin.py
+++ base/base_bot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class BotAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'cake_level', 'cake_form', 'cake_topping', 'cake_berries', 'cake_dekor', 'cake_inscription', 'cake_comment']
+    list_editable = ['cake_level', 'cake_form', 'cake_topping', 'cake_berries', 'cake_dekor', 'cake_inscription', 'cake_comment']
+
+admin.site.register(models.Cake, BotAdmin)
--- base/base_bot/models.py
+++ base/base_bot/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.timezone import localtime
 
-# Create your models here.
+
+class Cake(models.Model):
+    NUMBER_LEVELS = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3')
+    ]
+    FORM = [
+        ('Круг', 'КРУГ'),
+        ('Квадрат', 'КВАДРАТ'),
+        ('Прямоугольник', 'ПРЯМОУГОЛЬНИК')
+    ]
+    TOPPING = [
+        ('Без', 'БЕЗ'),
+        ('Белый соус', 'БЕЛЫЙ СОУС'),
+        ('Карамельный', 'КАРАМЕЛЬНЫЙ'),
+        ('Кленовый', 'КЛЕНОВЫЙ'),
+        ('Черничный', 'ЧЕРНИЧНЫЙ'),
+        ('Молочный шоколад', 'МОЛОЧНЫЙ ШОКОЛАД'),
+        ('Клубничный', 'КЛУБНИЧНЫЙ'),
+    ]
+    BERRIES = [
+        ('Ежевика', 'ЕЖЕВИКА'),
+        ('Малина', 'МАЛИНА'),
+        ('Голубика', 'ГОЛУБИКА'),
+        ('Клубника', 'КЛУБНИКА'),
+    ]
+    DEKOR = [
+        ('Фисташки', 'ФИСТАШКИ'),
+        ('Безе', 'БЕЗЕ'),
+        ('Фундук', 'ФУНДУК'),
+        ('Пекан', 'ПЕКАН'),
+        ('Маршмеллоу', 'МАРШМЕЛЛОУ'),
+        ('Марципан', 'МАРЦИПАН')
+    ]
+
+    cake_level = models.CharField(verbose_name='level', max_length=1, choices=NUMBER_LEVELS, default='1')
+    cake_form = models.CharField(verbose_name='form', max_length=20, choices=FORM)
+    cake_topping = models.CharField(verbose_name='topping', max_length=50, choices=TOPPING)
+    cake_berries = models.CharField(verbose_name='berries', max_length=20, blank = True, choices=BERRIES)
+    cake_dekor = models.CharField(verbose_name='dekor', max_length=20, blank = True, choices=DEKOR)
+    cake_inscription = models.CharField(verbose_name='inscription', max_length=100, blank = True)
+    cake_comment = models.CharField(verbose_name='comment', max_length=255, blank = True)
+
+    def __str__(self):
+        return f'{self.cake_level} {self.cake_form} {self.cake_topping} {self.cake_berries} {self.cake_dekor} {self.cake_inscription} {self.cake_comment}'
+
+
+# class Сake_buyer(models.Model):
+#     cake = models.ForeignKey(Cake, on_delete=models.CASCADE)
+#     buyer_name = models.CharField('ФИО', max_length=200)
+#     buyer_mail = models.EmailField(max_length=254)
+#     buyer_phone = models.CharField('промокод', max_length=10)
+#     buyer_date = models.DateField('дата заказа', null=True)
+#     buyer_discount = models.CharField('промокод', max_length=10, blank = True)

184b44aaa7d339bbd258de5b3f8fce1857cc2ccf
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-11-28 01:20:51 +0300

    добавил функции для клиента
    реализованы формально, завтра срочно пилим.

--- drf/drf/client.py
+++ drf/drf/client.py
@@ -7,3 +7,45 @@ def post_cake(payload):
     response.raise_for_status()
 
     return response.json()
+
+
+# в джанго можно сделать их методами класса Order
+def create_order(order_info: dict) -> Order:
+    '''обработчик для оформления заказа'''
+    pass
+
+
+def update_order(order_id: int, order_info: dict) -> Order:
+    pass
+
+
+def get_orders(user_id: int) -> list[Order]:
+    '''обработчик для ветки "повторить заказ"'''
+    pass
+
+
+def get_current_orders(user_id: int) -> list[Order]:
+    '''обработчик для ветки "где мой заказ"'''
+    pass
+
+# в джанго можно сделать их методами класса Cake
+def get_catalog_cakes() -> list[Cake]:
+    '''обработчик для ветки "каталог"'''
+    pass
+
+
+def get_random_cake() -> Cake:
+    '''обработчик для ветки "Удиви меня"'''
+    pass
+
+
+def create_cake(cake_info: dict) -> Cake:
+    '''обработчик для ветки "Создать торт"'''
+
+
+def create_user(user_info: dict) -> User:
+    pass
+
+
+def update_user(user_info: dict) -> User:
+    pass
